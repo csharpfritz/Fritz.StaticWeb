fd058186dfee7558f7c44845b0b24bc1a557e3ff
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2022-10-12 11:50:22 -0400

    Started adding web properties

---
+++ Fritz.StaticBlog/LocalWeb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.FileProviders;
+using NUglify.JavaScript.Syntax;
+
+public static class LocalWeb
+{
+
+  public static void StartAdminWeb(string[] args)
+  {
+
+    var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+    {
+      EnvironmentName = Environments.Production
+    });
+
+    builder.WebHost.UseUrls(new[] { "http://localhost:8028", "http://localhost:8029" });
+    builder.Logging.ClearProviders();
+
+    var app = builder.Build();
+
+    app.MapAdminSite();
+
+    app.Run();
+
+  }
+
+  public static IApplicationBuilder MapAdminSite(this IApplicationBuilder app)
+  {
+
+    app.MapWhen(ctx => ctx.Connection.LocalPort == 8028, config =>
+    {
+
+      config.MapWhen(ctx => ctx.Request.Path == "/", c => c.Run(x =>
+      {
+        x.Response.Redirect("/index.html");
+        return Task.CompletedTask;
+      }));
+
+      config.UseStaticFiles(new StaticFileOptions
+      {
+        FileProvider = new ManifestEmbeddedFileProvider(typeof(LocalWeb).Assembly, "adminweb")
+      });
+
+    });
+
+    return app;
+
+  }
+
+}
