83d9783762c4b04203e83681a5e9c591b0c86eae
Daniel W <bobfbaby@gmail.com>
2023-01-03 16:41:59 +0000

    Add bootstrap icons (#18)
    Replace text of nav items with bootstrap icons
    To achieve highlighting of current nav item, added an AnchorTagHelper which sets an active class when page matches link page

---
+++ Fritz.StaticBlog/adminweb/Helpers/ActiveTagHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Fritz.StaticBlog.adminweb.Helpers;
+
+[HtmlTargetElement(Attributes = "is-active-page")]
+public class ActiveTagHelper: AnchorTagHelper
+{
+  public ActiveTagHelper(IHtmlGenerator generator) : base(generator) { }
+        
+  public override void Process(TagHelperContext context, TagHelperOutput output)
+  {
+    
+    if(Page == null || ViewContext.RouteData.Values["page"] == null)
+      return;
+
+    if (string.Equals(Page, ViewContext.RouteData.Values["page"].ToString(),
+          StringComparison.InvariantCultureIgnoreCase))
+    {
+      var existingClasses = output.Attributes["class"].Value.ToString();
+      if (output.Attributes["class"] != null)
+      {
+        output.Attributes.Remove(output.Attributes["class"]);
+      }
+
+      output.Attributes.Add("class", $"{existingClasses} active");
+    } 
+    
+  }
+}
