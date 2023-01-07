88ce6dfe37d5520969c1cbe7f06fbc7a43880d8e
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2023-01-07 11:43:10 -0500

    Added marker test

--- Test.StaticBlog.Web/FirstTimeStartupAdminSiteFixture.cs
+++ Test.StaticBlog.Web/FirstTimeStartupAdminSiteFixture.cs
@@ -20,6 +20,15 @@ public class FirstTimeStartupAdminSiteFixture : BaseSiteFixture
 	}
 
 	[Test]
+	public async Task StartWithOnlyWorkingDirectoryShouldLoadSettingsFromDisk()
+	{
+
+		Assert.Fail("Test not written yet");
+
+	}
+
+
+	[Test]
 	public async Task SubmittingInvalidFolderShouldError()
 	{
 		await Page.GotoAsync("http://localhost:8028");
